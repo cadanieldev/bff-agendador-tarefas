7bd95bee8440588eefe3fee4c60cf39da1a83dce
Daniel <carlosdanielsantos458@gmail.com>
2026-09-16 14:02:49 -0300

    adicionado Cron

--- src/main/java/com/javanauta/bff_agendadortarefas/infrastructure/client/EmailClient.java
+++ src/main/java/com/javanauta/bff_agendadortarefas/infrastructure/client/EmailClient.java
@@ -2,11 +2,12 @@ package com.javanauta.bff_agendadortarefas.infrastructure.client;
 
 import com.javanauta.bff_agendadortarefas.business.dto.out.TarefasDTOResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "notificacao", url = "${notificacao.url}") // local para armazenar url e adicionar na properties
 public interface EmailClient {
-
+    @PostMapping
     void enviarEmail(@RequestBody TarefasDTOResponse dto);
 
 
